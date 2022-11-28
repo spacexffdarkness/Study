0f485d61f9658459b270b68c1c109d2679a595c0
Space <alexchernenko.ua@gmail.com>
2022-11-28 22:45:49 +0200

    First try

---
+++ Lesson 2.py
@@ -0,0 +1,14 @@
+print("import antigravity \nimport __hello__ \nfrom __future__ import braces")
+
+print('    * \t*\t **** *   *   *\t *   * ***  **** ****\n   * *  *\t **\t   * *\t * * * * * *  * **   ****\n  *   * **** **** *\t  * *\t**   * ***  **** *  *')
+
+
+print("""Escape sequences
+\a      Bell (alert)
+\b      Backspace
+\n      New line
+\t      Horizontal tab
+\\      Backslash \
+\"      Double quotation mark "
+\'      Single quotation mark '""")
+
