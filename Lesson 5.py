7e6ce92f281df33b0911283e1bf96dd378844ff9
Space <alexchernenko.ua@gmail.com>
2022-12-13 22:26:45 +0200

    First task done

---
+++ Lesson 5.py
@@ -0,0 +1,21 @@
+# first task
+my_string = '0123456789'
+n = 0
+i = 0
+while i <= 9:
+    num1 = my_string[i]
+    i += 1
+    for h in my_string:
+        num2 = my_string[n]
+        num = num1 + num2
+        print(num)
+        n += 1
+        if n > 9:
+            n = 0
+
+
+# second task
+# A
+# B
+# C
+# D
